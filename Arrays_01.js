718f78b5bb611e39e6ca991ff163b5b3ca4efe03
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-01-30 20:51:42 +0530

    array lecture

--- Arrays_01.js
+++ Arrays_01.js
@@ -9,7 +9,7 @@ const myArr1 = [1,2, 3, 4 ,5];
 // // zero based indexing
 
 // // Interview ques
-// // SHALLOW COPYING AND DEEP COPY
+// // ----------------------------SHALLOW COPYING AND DEEP COPY
 // // shallow copy means the original reference of the array and deep copy means the copy of original data 
 
 // console.log(myArr1[2]);
@@ -31,14 +31,14 @@ const myArr1 = [1,2, 3, 4 ,5];
 // // is there that index present
 // myArr1.indexOf(4);
 
-// // ---- JOIN (it will convert array in string  ) BASICALLY IT JOINS ALL THE ELEMENTS IN A STRING
+// // --------------------- JOIN (it will convert array in string  ) BASICALLY IT JOINS ALL THE ELEMENTS IN A STRING
 
 // const newarr = myArr1.join()
 // console.log(newarr);
 // console.log(myArr1);
 // console.log(typeof newarr);
 
-// //  SLICE, SPLICE
+// //  ---------------------------------SLICE, SPLICE
 // // SLICE returns section of a array (excluding last)
 // // MAJOR DIFFERENCE IS : splice cuts the section and changes in the original array
 // // SPLICE return the section of array (including last)
@@ -55,4 +55,57 @@ const myArr1 = [1,2, 3, 4 ,5];
 
 // console.log("old B array", b);
 // console.log("NEW B array", newB);
+
+// // ---------------------IF we try to push an array in another array then instead of adding that array elementr to last of that array it actually takes the complete array as an element and add that array as a single elemnt. (array inside array)
+
+// const a = [1,2,3,4,5,6,7];
+// const b = [1,2,3,4,5,6,7];
+
+// a.push(b);
+// console.log(a);
+
+
+// // Instead we will use---------- concat() ----------------function to merge the elements of both Arrays CONCAT function return the new array after merging the elements 
+
+
+// const a = [1,2,3,4,5,6,7];
+// const b = [1,2,3,4,5,6,7];
+
+// const c = a.concat(b);
+// console.log(c);
+
+// // --------------- SPREAD -------------
+// // we can use spread function also to spread the elements(merging arrays)
+
+// const a = [1,2,3,4,5,6,7];
+// const b = [1,2,3,4,5,6,7];
+
+// const c = [...a, ...b];
+// console.log(c);
+
+// //---------------------- If there is array inside array inside array, SO if you want to have all the elements in one array only them using FLAT METHOD---------------------
+
+// const arr1 = [1,2,3,4, [5,6], 7, [1,3,5,[7,9]]];
+// // const arr2 = arr1.flat(Infinity);
+
+// // WE CAN SAY INFINITY IF WE DON'T KNOW TYE DEPTH IF WE KNOW THEN WRITE ITB IN numbers, here depth is 3
+// // so
+
+// const arr2 = arr1.flat(3);
+
+// //---------------------------------- USED IN WEB SCRAPING
+
+// console.log(Array.isArray("utk"))
+// console.log(Array.from("utk")) //IT will convert data in array
+
+// // INTERESTING CASE FOR INTERVIEW (return empty array bcoz we need to provide what to have in array KEY or VALUE)
+// console.log(Array.from({name: "utk"})) 
+
+// //---------------------------- If you want to turn the multiple variales into an array
+
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+
+// console.log(Array.of(score1,score2,score3))
 
