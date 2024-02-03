acea6e70e49ad67e2d7479cdc66178558b50b6b0
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-02-03 12:13:36 +0530

    arrowFunction lecture

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,4 +1,5 @@
-// Arrow function
+
+//+++++++++++++++ Arrow function ++++++++++++++
 
 // this can't be used inside arrow function
 const abc = () => {
@@ -7,3 +8,20 @@ const abc = () => {
     console.log(this.name) // undefined
 }
 
+//  There is another way also to declare arrow function  (Implicit return) : we need not to write curly brackets here, we can simply put it onto one line code
+
+// if written in curly braces we need to write the return keyword but if we are not using it then no need to write return keyword and also if using small brackets thyen also no need to write the return keyword
+
+const addTwo = (num1, num2) => num1 + num2
+
+// alternate way to write it :-
+
+// const addTwo = (num1, num2) => (num1 + num2)
+console.log(addTwo(3,4));
+
+// if you want to add objects in this function then ->
+
+// we need to pass it in small brackets, coz it will be undefined if we will not use "()"
+// const addfunc = () => {name: "s"} //undefined
+
+const addfunc = () => ({name: "s"})
