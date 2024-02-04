c6a27ea8f515cf7eccd14238d4e8ea5d43606808
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-02-04 14:03:55 +0530

    controlFlow lecture

---
+++ Controlflow.js
@@ -0,0 +1,20 @@
+//  Logic control / control flow
+
+// all the code should not be run at a time, means it should be in sequence what should run first and then what should run after that , so this is done by control flow
+
+// 1---- if else
+
+if(true){ // conditions are checked here
+
+}
+
+
+//  In switch case without break all the cases will execute except the default case :-
+
+// true and false are strictly true/false values but truthy and falsy values are assumed to be true or false:
+
+// truthy values
+// "0" , 'false'/"false", " " //space agr h quotes, [], {}, function(){}
+
+// falsy values
+// false, 0 , -0, Bigint -- 0n, "", null , undefined, NaN
