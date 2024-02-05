a55513b5b8e6cff37c2805cce4e3f1411a4f688d
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-02-05 23:10:56 +0530

    advanceJS lecture

--- CoreJS/object01.js
+++ CoreJS/object01.js
@@ -1,109 +1,109 @@
-//  Two ways to create object
-// literal
-//  constructor
+// //  Two ways to create object
+// // literal
+// //  constructor
 
-// when object is creatred by constructor themn its SINGLETON means uske jaisa srf ek hi object h
-// when we declare object from literals then no singleton is created
+// // when object is creatred by constructor themn its SINGLETON means uske jaisa srf ek hi object h
+// // when we declare object from literals then no singleton is created
 
-// objects is also made by this method (this is for singleton/constructor)
-// Object.create 
+// // objects is also made by this method (this is for singleton/constructor)
+// // Object.create 
 
-// object literals
+// // object literals
 
-const obj1 = {
-     name : "hey",
-       "sur name" : "shar",//To access this we cant use dot operator
-     age : 234,
-     grp : " 0 +ve"
-}
+// const obj1 = {
+//      name : "hey",
+//        "sur name" : "shar",//To access this we cant use dot operator
+//      age : 234,
+//      grp : " 0 +ve"
+// }
 
-// name , age , grp all these keys are passed as an string, means its like "name", "age", "grp", it takes this keys as string b y default
+// // name , age , grp all these keys are passed as an string, means its like "name", "age", "grp", it takes this keys as string b y default
 
-// Accessing objects can be done like: 
+// // Accessing objects can be done like: 
 
-console.log(obj1.name);
+// console.log(obj1.name);
 
-// console.log(obj1.sur name); // we cant access in this way
+// // console.log(obj1.sur name); // we cant access in this way
 
-console.log(obj1["name"]);  // if key is given in a string and to acces also we have to pass the key in dounle quote(as a string)
+// console.log(obj1["name"]);  // if key is given in a string and to acces also we have to pass the key in dounle quote(as a string)
 
-console.log(obj1["sur name"]); // this is the only way left to access
+// console.log(obj1["sur name"]); // this is the only way left to access
 
-// ----------------------INTERVIEW QUESTION-------------------
-// if we wanna uswe the symbol as a key in object tyhen how can we use it ?
+// // ----------------------INTERVIEW QUESTION-------------------
+// // if we wanna uswe the symbol as a key in object tyhen how can we use it ?
 
-const mysym = Symbol("key1");
+// const mysym = Symbol("key1");
 
-const obj1 = {
-         name : "hey",
-           "sur name" : "shar",//To access this we cant use dot operator
-         age : 234,
-         grp : " 0 +ve",
-         mysym  : "key1"
-    }
+// const obj1 = {
+//          name : "hey",
+//            "sur name" : "shar",//To access this we cant use dot operator
+//          age : 234,
+//          grp : " 0 +ve",
+//          mysym  : "key1"
+//     }
 
-    console.log(typeof obj1.mysym);  
+//     console.log(typeof obj1.mysym);  
 
-    // When you do like this then this is wrong because it is taken as a string not a Symbol so if you wannt to take it as a symbol then
-
-
-const mysym = Symbol("key1");
-
-const obj1 = {
-         name : "hey",
-           "sur name" : "shar",//To access this we cant use dot operator
-         age : 234,
-         grp : " 0 +ve",
-         [mysym]  : "key1"     // using square brackets
-    }
-
-    console.log(typeof obj1[mysym]);    // using square brackets
-
-    // Using square brackets only we can use symbols a s key inside object
+//     // When you do like this then this is wrong because it is taken as a string not a Symbol so if you wannt to take it as a symbol then
 
 
-//  ----------------- If we want to change the value of any key --------
+// const mysym = Symbol("key1");
 
-const obj1 = {
-         name : "hey",
-            "sur name" : "shar",
-          grp : " 0 +ve",
+// const obj1 = {
+//          name : "hey",
+//            "sur name" : "shar",//To access this we cant use dot operator
+//          age : 234,
+//          grp : " 0 +ve",
+//          [mysym]  : "key1"     // using square brackets
+//     }
 
-    }
+//     console.log(typeof obj1[mysym]);    // using square brackets
 
-    obj1.name = "myName"; // In this way we can change the value of a key
+//     // Using square brackets only we can use symbols a s key inside object
 
-    console.log(obj1);
 
-    // And if you dont wanna allow to change the value, we can lock it
+// //  ----------------- If we want to change the value of any key --------
 
-    Object.freeze(obj1); // Now we can't chnage anything and its not going to give error 
+// const obj1 = {
+//          name : "hey",
+//             "sur name" : "shar",
+//           grp : " 0 +ve",
 
-const obj1 = {
-         name : "hey",
-            "sur name" : "shar",
-          grp : " 0 +ve",
-    }
+//     }
 
-    // functions can be used as a variable in objects
+//     obj1.name = "myName"; // In this way we can change the value of a key
 
-    obj1.greet = function()
-    {
-      console.log("HIIIIIIIII");
-    }
-    console.log(obj1.greet)  // function execute nhi hua h sirf uska reference aayega 
+//     console.log(obj1);
 
-    console.log(obj1.greet()) // yaha pr execute hua 
+//     // And if you dont wanna allow to change the value, we can lock it
 
-//--------------- If you want to take the key and use it in a function of object
+//     Object.freeze(obj1); // Now we can't chnage anything and its not going to give error 
 
-    const obj1 = {
-      name : "hey",
-         "sur name" : "shar",
-       grp : " 0 +ve",
- }
+// const obj1 = {
+//          name : "hey",
+//             "sur name" : "shar",
+//           grp : " 0 +ve",
+//     }
 
- obj1.greet = function(){
-  console.log(`hi user ${this.name}`) // 'this' keyword refers to the same object you are referencing
- }
+//     // functions can be used as a variable in objects
+
+//     obj1.greet = function()
+//     {
+//       console.log("HIIIIIIIII");
+//     }
+//     console.log(obj1.greet)  // function execute nhi hua h sirf uska reference aayega 
+
+//     console.log(obj1.greet()) // yaha pr execute hua 
+
+// //--------------- If you want to take the key and use it in a function of object
+
+//     const obj1 = {
+//       name : "hey",
+//          "sur name" : "shar",
+//        grp : " 0 +ve",
+//  }
+
+//  obj1.greet = function(){
+//   console.log(`hi user ${this.name}`) // 'this' keyword refers to the same object you are referencing
+//  }
 
--- CoreJS/object02.js
+++ CoreJS/object02.js
@@ -1,104 +1,104 @@
-//  OBJECT SUNGLETON OR USING CONSTRUCTOR
+// //  OBJECT SUNGLETON OR USING CONSTRUCTOR
 
-// both are objects but using new keyword its a singleton object
-const obj1 = {}     // literal object
-const obj2 = new Object()   // singleton object
+// // both are objects but using new keyword its a singleton object
+// const obj1 = {}     // literal object
+// const obj2 = new Object()   // singleton object
 
-// otherwise there is no differemce in them
+// // otherwise there is no differemce in them
 
-//  ------------NESTING OF OBJECTS(object inside objects)------------------
+// //  ------------NESTING OF OBJECTS(object inside objects)------------------
 
-const obj3 = {
-    email: "hi@gmail.com",
-    fullname :{
-       userfullname  : {
-        firstname  :" utk ",
-        lastname : " sha ",
-       }
-    }
-}
+// const obj3 = {
+//     email: "hi@gmail.com",
+//     fullname :{
+//        userfullname  : {
+//         firstname  :" utk ",
+//         lastname : " sha ",
+//        }
+//     }
+// }
 
-// Accesing values 
+// // Accesing values 
 
-console.log(obj3.fullname.userfullname.firstname);
+// console.log(obj3.fullname.userfullname.firstname);
 
-// If in case fullname doesnt exist then it will protect us
+// // If in case fullname doesnt exist then it will protect us
 
-console.log(obj3.fullname?.userfullname.firstname);
+// console.log(obj3.fullname?.userfullname.firstname);
 
-// ===================  COMBINING  OBJECTSS ====================== 
+// // ===================  COMBINING  OBJECTSS ====================== 
 
-const obj1 = { 1 : "a", 2 : "b"}
-const obj2 = { 3 : "c", 4 : "d"}
+// const obj1 = { 1 : "a", 2 : "b"}
+// const obj2 = { 3 : "c", 4 : "d"}
 
-const obj3 = { obj1, obj2}
-console.log(obj3);
-
-
-//  In this situation again the same problem we havent concationate the vlues of the objects it is combined like objects having two diffret objects
-
-const obj4 = Object.assign( {}, obj1 , obj2);
-
-// for combing them we will use this. It will return a modified object after combing them in a new object 
-
-// --------------ONE MORE WAY ---------------------------
-//  using spread operator
+// const obj3 = { obj1, obj2}
+// console.log(obj3);
 
 
-const obj1 = { 1 : "a", 2 : "b"}
-const obj2 = { 3 : "c", 4 : "d"}
+// //  In this situation again the same problem we havent concationate the vlues of the objects it is combined like objects having two diffret objects
 
-const obj3 = {...obj1, ...obj2}
-console.log(obj3)
+// const obj4 = Object.assign( {}, obj1 , obj2);
 
-// Whenever data comes from database it usually comes in format of  (array of objects)
+// // for combing them we will use this. It will return a modified object after combing them in a new object 
 
-const users = [
-    {
-        id :1,
-        email : "her@gmail.com"
-    },
-    {
-        id :2,
-        email : "him@gmail.com"
-    },
-    {
-        id :3,
-        email : "his@gmail.com"
-    }
-]
+// // --------------ONE MORE WAY ---------------------------
+// //  using spread operator
 
-// If let say we have to loop from this array (using map) or we have to print  a value
 
-console.log(users[1].email) // This will print the first object's email from the array
+// const obj1 = { 1 : "a", 2 : "b"}
+// const obj2 = { 3 : "c", 4 : "d"}
 
-//-----------------------------Important concept-------------------------
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3)
 
-const obj1 = {
-    id: 1,
-    class: "3",
-    name : "hier",
-    grp : "o +ve"
-}
+// // Whenever data comes from database it usually comes in format of  (array of objects)
 
-// Object.keys will return all the keys of object inn an array
-console.log(Object.keys(obj1));
-// OUTPUT-  [ 'id', 'class', 'name', 'grp' ]
-
-// Object.values will return all the values of object inn an array
-console.log(Object.values(obj1));
-// OUTPUT-  [ 1, '3', 'hier', 'o +ve' ]
-
-// Object.entris will return all the key & values of object inn an array (array inside array)
-
-console.log(Object.entries(obj1));
-// OUTPUT-  [
-//   [ 'id', 1 ],
-//   [ 'class', '3' ],
-//   [ 'name', 'hier' ],
-//   [ 'grp', 'o +ve' ]
+// const users = [
+//     {
+//         id :1,
+//         email : "her@gmail.com"
+//     },
+//     {
+//         id :2,
+//         email : "him@gmail.com"
+//     },
+//     {
+//         id :3,
+//         email : "his@gmail.com"
+//     }
 // ]
 
-//  if we want to know is there any property named " nameOftheProperty", it will return true or flase
-console.log(obj1.hasOwnProperty("grp")); //true
-console.log(obj1.hasOwnProperty("grpppp")); //false
+// // If let say we have to loop from this array (using map) or we have to print  a value
+
+// console.log(users[1].email) // This will print the first object's email from the array
+
+// //-----------------------------Important concept-------------------------
+
+// const obj1 = {
+//     id: 1,
+//     class: "3",
+//     name : "hier",
+//     grp : "o +ve"
+// }
+
+// // Object.keys will return all the keys of object inn an array
+// console.log(Object.keys(obj1));
+// // OUTPUT-  [ 'id', 'class', 'name', 'grp' ]
+
+// // Object.values will return all the values of object inn an array
+// console.log(Object.values(obj1));
+// // OUTPUT-  [ 1, '3', 'hier', 'o +ve' ]
+
+// // Object.entris will return all the key & values of object inn an array (array inside array)
+
+// console.log(Object.entries(obj1));
+// // OUTPUT-  [
+// //   [ 'id', 1 ],
+// //   [ 'class', '3' ],
+// //   [ 'name', 'hier' ],
+// //   [ 'grp', 'o +ve' ]
+// // ]
+
+// //  if we want to know is there any property named " nameOftheProperty", it will return true or flase
+// console.log(obj1.hasOwnProperty("grp")); //true
+// console.log(obj1.hasOwnProperty("grpppp")); //false
