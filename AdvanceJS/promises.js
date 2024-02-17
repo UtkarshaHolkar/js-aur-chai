d44923176631ff800eeb4451b624995813b13ab2
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-02-17 23:01:14 +0530

    advanceJS lecture

--- AdvanceJS/promises.js
+++ AdvanceJS/promises.js
@@ -1,4 +1,4 @@
-    //----------------PROMISES ------------------
+//----------------------PROMISES ------------------
     
     // ES6 m promise aya
     //define
@@ -13,5 +13,38 @@
     // 3. rejected
 
     //creating a promise---- 
+// promise ek callback leta h apne andar: 
 
-const promise1 = new Promise();
+// ek resolve ek reject do part hote h function k
+
+const promise1 = new Promise(function(resolve, reject ){
+    // what type of work can be done hrer??
+    // 1) do an async task 
+    // 2) db ko calls 
+    // 3) network call
+    setTimeout(() => {
+        console.log('async task is completed');
+        resolve() // bina iss line k .then s vo connect nhi hoga (promise consume bhi nhi hoga )
+    }, 1000); 
+
+});
+
+// ab promise create krne k baad isko consume bhi krna pdhega -- 
+
+// .then k sidha connection h resolve k sath
+
+promise1.then(function(){
+    console.log('promise consumed');
+})
+
+
+// another way to create and consume
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log('async task 2');
+        resolve()
+    },1000)
+}).then(function(){
+    console.log("async 2 resolved");
+})
