96026b5270cf887ccf11f329ab501acbfe07586f
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-02-17 22:17:20 +0530

    advanceJS lecture

---
+++ AdvanceJS/promises.js
@@ -0,0 +1,17 @@
+    //----------------PROMISES ------------------
+    
+    // ES6 m promise aya
+    //define
+
+    // promise ek object h jo represent krta h eventual completion or failure
+
+    // it simply means jo bhi task h ab vo queue m add hogya h pr vo abhi k abhi complete nhi hoga
+    
+    // states - a promise is in one of the states:
+    // 1. pending
+    // 2. fulfilled
+    // 3. rejected
+
+    //creating a promise---- 
+
+const promise1 = new Promise();
