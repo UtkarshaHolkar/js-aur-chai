af3f340286bea3de51fb1ca08c2f92c2477b4dd5
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-02-19 11:48:33 +0530

    advanceJS lecture

--- AdvanceJS/fetch.js
+++ AdvanceJS/fetch.js
@@ -1,5 +1,14 @@
 //+++++++++++++++ FETCH +++++++++++++++++++++
 
+fetch('http://jsonplaceholder.typicode.com/users').then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((e)=> console.log(e))
+
+
 // ye ek global method h jo ki resource extract / fetch krti h from the network...
 // ye ek promise return krta h.
 
@@ -17,3 +26,9 @@
 //Thats the reason firstly fetch code ran and output was shown ...
 
 // fetch--- domparts m kaam krta h
+// 1. web browser/ node -- api / request handle krta h ye--
+// ye ya toh browser based api handle krega ya fir node based api.. yaha se ek network request milti h, mtlb ya toh network pr req jayegi ya fir nahi jayegi.. agr req chli gyi or vaha se response aagya toh vo humesha onFulfilled[] m jata h.. or agr response nhi aata h ya fir req ja hi nhi payi toh onRejection[] m aayega, agr 404 ya kisi bhi tarah k error response aata h tb bhi vo onfulfilled[] m jayega because ye ek response h req se...
+
+// 2. data -- ek variables m space reserve krne k liye, jisme ek array hoti h onFulfilled[] or dusri array hoti h onRejection[], isme hum manually kuch bhi push nhi kr skte h, access hi nhi kr skte h isse
+
+// firstly data reserve hga memory m then api req handle hogi
